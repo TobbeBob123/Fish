fb5249904eedab075fefa750876ea31dbab154e0
Tobias <tobias.finsbol@gmail.com>
2022-04-14 01:19:58 +0200

    Endret alias

--- config.fish
+++ config.fish
@@ -1,9 +1,6 @@
 # Fjern Fish intro
 set fish_greeting
 
-# Send huskelap vær gang terminal (Fish åpnes)
-~/script/husk_oppdater.sh
-
 # Tilfeldig COLOR SCRIPT 
 colorscript random
 
@@ -15,13 +12,16 @@ alias pau='sudo pacman -Syyu'
 alias pai='sudo pacman -S'
 alias paf='sudo pacman -Rs'
 alias pay='sudo pacman -Syy'
-alias reboot='sudo reboot 0'
-alias shutdown='shutdown 0'
+alias reboot='sudo reboot now'
+alias shutdown='sudo shutdown now'
 alias yay='paru'
 alias ys='paru -S'
 alias y='paru -Syu'
 alias yf='paru -Rs'
 alias icat='kitty +kitten icat'
+alias x=~/Script/HDMI1.sh
+alias z=~/Script/LaptopBare.sh
+alias awkp='awk {print}'
 
 #Start starship
 starship init fish | source
