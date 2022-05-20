66a8a41c81c24a0aee45020350a0e1716a217f86
Tobias <tobias.finsbol@gmail.com>
2022-05-20 12:40:49 +0200

    script to show aliases

--- alias.sh
+++ alias.sh
@@ -1,1 +1,1 @@
-sed -n '/Start_alias/,/End_alias/p' ~/.config/fish/config.fish | grep -v '#End_' | grep -v '#Start_' | yad --text-info --back=#282a36 --fore=#ff76c6 --geometry=800x500
+sed -n '/Start_alias/,/End_alias/p' ~/.config/fish/config.fish | grep -v '#End_' | grep -v '#Start_' | sed -e 's/alias /\n/' | yad --text-info --back=#282a36 --fore=#ff76c6 --geometry=800x500
